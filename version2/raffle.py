db60e793d14a83745cb5bca19ff9961b2161a6e9
Wooyang Son <sonwy102@gmail.com>
2020-10-08 20:08:19 +0000

    Completed raffle.py version2

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,1 +1,16 @@
 """Randomly pick customer and print customer info"""
+
+from random import choice
+import customers
+
+def print_random_customer(customers_file_path):
+    """Take txtfile of customers and print random customer's info"""
+
+    # Generate list of all customer objects from file
+    customers_list = customers.get_customers_from_file(customers_file_path)
+
+    # Choose random customer object from list
+    rand_customer = choice(customers_list)
+    
+    # Print all of random customer's attributes
+    print(rand_customer.__dict__)
